4489b100f72b2c92c4d5a91de4441f25f4367d48
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-11-10 16:29:42 -0300

    added a swiftUI view

--- Pitang Agile IT/AppFront/Clousures/ClousuresView.swift
+++ Pitang Agile IT/AppFront/Clousures/ClousuresView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ClousuresView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        
+    
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.system(size: 20, weight: .bold))
+        
+        Button("go Back") {
+            
+            presentationMode.wrappedValue.dismiss()
+        }.foregroundColor(.white)
+         .font(.system(size: 20, weight: .bold))
     }
 }
