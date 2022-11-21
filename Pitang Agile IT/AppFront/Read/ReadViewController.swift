fdce52e37ccb6e9ded21ce15d6c6f8468fe5ec8d
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-11-21 11:50:06 -0300

    added a button to go to batata page

--- Pitang Agile IT/AppFront/Read/ReadViewController.swift
+++ Pitang Agile IT/AppFront/Read/ReadViewController.swift
@@ -6,28 +6,31 @@
 //
 
 import UIKit
+import SafariServices
 
 class ReadViewController: UIViewController {
     private let label = Create.label("Read-Me", font: Assets.font(30),
                                      alignment: .center)
     private let rtfFormat = [NSAttributedString.DocumentReadingOptionKey.documentType:
                                 NSAttributedString.DocumentType.rtf]
-    private let stack: UIStackView = {
+    private lazy var stack: UIStackView = {
         
-        let firstBbutton = Create.button("Tap-Me") {_ in
-            print("Toma!")
-        }
+        let firstBbutton = Create.button("Batatinha")
+        firstBbutton.addTarget(self, action: #selector(buttonsTarget), for: .touchUpInside)
         firstBbutton.backgroundColor = .lightGray
         
-        let secondBbutton = Create.button("I have a dream") {_ in
-            print("Toma!")
-        }
+        let secondBbutton = Create.button("Batatas")
+        secondBbutton.addTarget(self, action: #selector(buttonsTarget), for: .touchUpInside)
         secondBbutton.backgroundColor = .lightGray
+        
+        let thirdyBbutton = Create.button("Go")
+        thirdyBbutton.addTarget(nil, action: #selector(goButtonTarget), for: .touchUpInside)
+        thirdyBbutton.backgroundColor = .systemBlue
         
         let label = Create.label("Read-Me", font: Assets.font(20),
                                  alignment: .center)
         
-        let stack = UIStackView(arrangedSubviews: [firstBbutton, secondBbutton, label])
+        let stack = UIStackView(arrangedSubviews: [firstBbutton, secondBbutton, thirdyBbutton, label])
         stack.axis = .vertical
         
         return stack
@@ -44,15 +47,21 @@ class ReadViewController: UIViewController {
         super.loadView()
         print(Network.read(Jason.self, from: "Jason") as Any)
         setup()
-        showBatata(file: "Batatinha")
-        textView.text.append("\n\n")
-        showBatata(file: "Batatas")
     }
     private func showBatata(file name: String) {
         guard let url = Bundle.main.url(forResource: name, withExtension: "rtf") else {return}
         
         guard let text = try? NSMutableAttributedString(url: url, options: rtfFormat, documentAttributes: .none) else {return}
-        textView.text.append(text.string)
+        textView.text = text.string
+    }
+    @objc func buttonsTarget(_ sender: UIButton) {
+        
+        showBatata(file: sender.currentTitle ?? "Batata")
+    }
+    @objc func goButtonTarget() {
+        guard let url = URL(string: textView.text ?? "https://pt.wikipedia.org/wiki/Batata") else {return}
+        let page = SFSafariViewController(url: url)
+        navigationController?.navigate(to: page)
     }
 }
 
